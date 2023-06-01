be4f946e4eea19a405b0c41d6e3f2c0569186fbe
Felipecordeiiro <imfelipe.cordeiro12@gmail.com>
2023-06-01 11:10:01 -0300

    protótipo_eye-tracker

---
+++ README.md
@@ -0,0 +1,2 @@
+Código completamente baseado no código do stepacool:"https://github.com/stepacool/Eye-Tracker".
+Protótipo v = 1.0
--- iris.py
+++ iris.py
@@ -1,22 +1,26 @@
 import cv2 as cv
 import numpy as np
 
-face_cascade = cv.CascadeClassifier('Eyetracking/haarcascade_frontalface_default.xml') 
-eye_cascade = cv.CascadeClassifier('Eyetracking/haarcascade_eye.xml') 
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml') 
+eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml') 
+detector_params = cv.SimpleBlobDetector_Params()
+detector_params.filterByArea = True
+detector_params.maxArea = 1500
+detector = cv.SimpleBlobDetector_create(detector_params)
 
-img = cv.imread('Eyetracking/Michael-B-Jordan-2019.jpg')
-gray_convert = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-detection_face = face_cascade.detectMultiScale(gray_convert,1.3,5)
+#img = cv.imread('Michael-B-Jordan-2019.jpg')
+#gray_convert = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#detection_face = face_cascade.detectMultiScale(gray_convert,1.3,5)
 
-for (x,y,w,h) in detection_face:
-    cv.rectangle(img, (x,y), (x+w,y+h), (255,255,0),2)
+#for (x,y,w,h) in detection_face:
+#    cv.rectangle(img, (x,y), (x+w,y+h), (255,255,0),2)
     
-gray_picture = gray_convert[y:y+h,x:x+w]
-face = img[y:y+h,x:x+w]
-detection_eyes = eye_cascade.detectMultiScale(gray_picture)
+#gray_picture = gray_convert[y:y+h,x:x+w]
+#face = img[y:y+h,x:x+w]
+#detection_eyes = eye_cascade.detectMultiScale(gray_picture)
 
-for (xe,ye,we,he) in detection_eyes:
-    cv.rectangle(face, (xe,ye), (xe+we,ye+he), (0,225,255),2)
+#for (xe,ye,we,he) in detection_eyes:
+#    cv.rectangle(face, (xe,ye), (xe+we,ye+he), (0,225,255),2)
 
 def separe_img(classifier,img,img_gray):
     coords = classifier.detectMultiScale(img_gray,1.3,5)
@@ -26,21 +30,26 @@ def separe_img(classifier,img,img_gray):
         if y+h > height/2:
             pass
 
-def detect_eye(classifier,img):
+def detect_eye(img,classifier):
     gray_frame = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
     eyes = classifier.detectMultiScale(gray_frame,1.3,5)
-    width = np.size(gray_frame,1)
-    height = np.size(gray_frame,0)
-    if y > height / 2:
-        pass
-        eyecenter = x + w / 2  # get the eye center
+    width = np.size(img,1)
+    height = np.size(img,0)
+    left_eye = None
+    right_eye = None
+
+    for (x,y,w,h) in eyes:
+        if y > height / 2:
+            pass
+        eyecenter = x + w / 2  # dividir a imagem em duas partes, assim pegando cada olho
         if eyecenter < width * 0.5:
             left_eye = img[y:y + h, x:x + w]
         else:
             right_eye = img[y:y + h, x:x + w]
-    return left_eye, right_eye
+    
+    return right_eye, left_eye
 
-def detect_faces(classifier,img):
+def detect_faces(img, classifier):
 
     gray_frame = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     coords = classifier.detectMultiScale(gray_frame,1.3,5)
@@ -58,12 +67,52 @@ def detect_faces(classifier,img):
     
     for (x,y,w,h) in biggest:
         frame = img[y:y+h,x:x+w]
+
     return frame
 
-detect_eye(eye_cascade, gray_convert)
-detect_faces(face_cascade, gray_convert)
+def cut_eyebrows(img):
+    height, width = img.shape[:2]
+    eyebrow_h = int(height / 4)
+    img = img[eyebrow_h:height, 0:width]  # cut eyebrows out (15 px)return img
 
-detector_params = cv.SimpleBlobDetector_Params()
-detector_params.filterByArea = True
-detector_params.maxArea = 1500
-detector = cv.SimpleBlobDetector_create(detector_params)
+    return img
+
+def blob_process(img,threshold,detector):
+
+    gray_frame = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    _, img = cv.threshold(gray_frame, threshold, 255, cv.THRESH_BINARY)
+    img = cv.erode(img, None, iterations=3)
+    img = cv.dilate(img, None, iterations=5)
+    img = cv.blur(img,(5,5))
+    keypoints = detector.detect(img)
+    print(keypoints)
+    return keypoints
+
+
+def nothing(x):
+    pass
+
+def main():
+    cap = cv.VideoCapture(0)
+    cv.namedWindow('image')
+    cv.createTrackbar('threshold', 'image', 0, 255, nothing)
+    cv.setWindowProperty('imageeeem',cv.WND_PROP_FULLSCREEN,cv.WINDOW_FULLSCREEN)
+    while True:
+        _, frame = cap.read()
+        face_frame = detect_faces(frame, face_cascade)
+        if face_frame is not None:
+            eyes = detect_eye(face_frame, eye_cascade)
+            for eye in eyes:
+                if eye is not None:
+                    threshold = cv.getTrackbarPos('threshold', 'image')
+                    eye = cut_eyebrows(eye)
+                    keypoints = blob_process(eye, threshold, detector)
+                    eye = cv.drawKeypoints(eye, keypoints, eye, (0, 0, 255), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        cv.imshow('image', frame)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
